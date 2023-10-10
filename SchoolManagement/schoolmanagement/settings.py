ff4ff51c6b28c7b64330568135e6ba9d870bdb91
Hamzat Adebayo Hamzat <hamzatadebayo5@gmail.com>
2023-10-10 13:18:19 +0100

    Update settings.py

--- SchoolManagement/schoolmanagement/settings.py
+++ SchoolManagement/schoolmanagement/settings.py
@@ -156,5 +156,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'ukanah15thdean@gmail.com'  # Replace with your email address
-EMAIL_HOST_PASSWORD = 'phoyxxbefvrnwoqg'  # Replace with your email password
+EMAIL_HOST_USER = ''  # Replace with your email address
+EMAIL_HOST_PASSWORD = ''  # Replace with your email password
